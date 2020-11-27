8e63d91ef7e4a75f601fd09faf0aa729c1dda61c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-27 12:20:37 +0000

    Deploy moesuiga/typescript-exercises to github.com/moesuiga/typescript-exercises.git:gh-pages

---
+++ precache-manifest.2982ca188a6d52fac62aff4475de09ad.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e200f6db9470f0a3c0a12a6411704785",
+    "url": "/typescript-exercises/7ee540a59183a6e576b4.worker.js"
+  },
+  {
+    "revision": "ce15c9d372cc5baeacdeb733817bdde4",
+    "url": "/typescript-exercises/7ee540a59183a6e576b4.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "494fa7137856f05058b9c6b41c986e00",
+    "url": "/typescript-exercises/editor.worker.js"
+  },
+  {
+    "revision": "d18f88ae272728f6d0313ab16ef13438",
+    "url": "/typescript-exercises/index.html"
+  },
+  {
+    "revision": "ed34a6d2b6fa43fd722d8dd73436935e",
+    "url": "/typescript-exercises/json.worker.js"
+  },
+  {
+    "revision": "c7fc75219a34acf710fc",
+    "url": "/typescript-exercises/static/css/3.f5145c50.chunk.css"
+  },
+  {
+    "revision": "5740b9790b6e43270064",
+    "url": "/typescript-exercises/static/js/0.fdca4b35.chunk.js"
+  },
+  {
+    "revision": "d3d7c3278ed6de5114e8",
+    "url": "/typescript-exercises/static/js/10.2f368d8c.chunk.js"
+  },
+  {
+    "revision": "f68d3d647c6f1d8ed4d4",
+    "url": "/typescript-exercises/static/js/11.2cc84311.chunk.js"
+  },
+  {
+    "revision": "6999224551c5b5601cb8",
+    "url": "/typescript-exercises/static/js/12.ab553806.chunk.js"
+  },
+  {
+    "revision": "20f8d07bc393771dba6f",
+    "url": "/typescript-exercises/static/js/13.e8231231.chunk.js"
+  },
+  {
+    "revision": "222cd83cbeba84517650",
+    "url": "/typescript-exercises/static/js/14.9c29dc14.chunk.js"
+  },
+  {
+    "revision": "69369618c590460307d7",
+    "url": "/typescript-exercises/static/js/15.1833d822.chunk.js"
+  },
+  {
+    "revision": "2f0c7540f503587eceae",
+    "url": "/typescript-exercises/static/js/16.73c9864e.chunk.js"
+  },
+  {
+    "revision": "d2f133c8c9ce99fec602",
+    "url": "/typescript-exercises/static/js/17.2ec286d6.chunk.js"
+  },
+  {
+    "revision": "0af1ec6ca497084a196e",
+    "url": "/typescript-exercises/static/js/18.c5684c44.chunk.js"
+  },
+  {
+    "revision": "a5ba9aa072083185e356",
+    "url": "/typescript-exercises/static/js/19.b8659aeb.chunk.js"
+  },
+  {
+    "revision": "3bbf03eedb956657c88d",
+    "url": "/typescript-exercises/static/js/20.b637573c.chunk.js"
+  },
+  {
+    "revision": "324bc7f641b4f0b5c9dc",
+    "url": "/typescript-exercises/static/js/21.877d5936.chunk.js"
+  },
+  {
+    "revision": "11a9e09038f21854f22e",
+    "url": "/typescript-exercises/static/js/22.db413ad0.chunk.js"
+  },
+  {
+    "revision": "877feb7f903bf31ff239",
+    "url": "/typescript-exercises/static/js/23.1fe161da.chunk.js"
+  },
+  {
+    "revision": "57077e125d51fcf51238",
+    "url": "/typescript-exercises/static/js/24.9f5d4b1d.chunk.js"
+  },
+  {
+    "revision": "77b5a9122a3680d389c9",
+    "url": "/typescript-exercises/static/js/25.7683a8d4.chunk.js"
+  },
+  {
+    "revision": "a197d96babf590cd2da4",
+    "url": "/typescript-exercises/static/js/26.c5ae49b4.chunk.js"
+  },
+  {
+    "revision": "640e044cb0beed179dac",
+    "url": "/typescript-exercises/static/js/27.aa81ae27.chunk.js"
+  },
+  {
+    "revision": "eadfa896faa43407d85c",
+    "url": "/typescript-exercises/static/js/28.53219ed2.chunk.js"
+  },
+  {
+    "revision": "33b08cd7f34a648e9896",
+    "url": "/typescript-exercises/static/js/29.59b86c37.chunk.js"
+  },
+  {
+    "revision": "c7fc75219a34acf710fc",
+    "url": "/typescript-exercises/static/js/3.ed21f883.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/typescript-exercises/static/js/3.ed21f883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7118a7224d3a06a641",
+    "url": "/typescript-exercises/static/js/30.91d1e338.chunk.js"
+  },
+  {
+    "revision": "2480e900d3fd16dbf501",
+    "url": "/typescript-exercises/static/js/31.df3f39ba.chunk.js"
+  },
+  {
+    "revision": "30e8cedb1786e79b103b",
+    "url": "/typescript-exercises/static/js/32.c5d1dc68.chunk.js"
+  },
+  {
+    "revision": "566950a5b37b68c53d6a",
+    "url": "/typescript-exercises/static/js/33.a98e02e3.chunk.js"
+  },
+  {
+    "revision": "4038f85d1daad7383b61",
+    "url": "/typescript-exercises/static/js/34.98a2eace.chunk.js"
+  },
+  {
+    "revision": "9498788e84a12ada71d8",
+    "url": "/typescript-exercises/static/js/35.86e9ded9.chunk.js"
+  },
+  {
+    "revision": "a5ebb46f77478493db71",
+    "url": "/typescript-exercises/static/js/36.fb831753.chunk.js"
+  },
+  {
+    "revision": "705bd5f39f5c92caa23a",
+    "url": "/typescript-exercises/static/js/37.b50c600c.chunk.js"
+  },
+  {
+    "revision": "0de1ff62d2e47205722a",
+    "url": "/typescript-exercises/static/js/38.3505e7b1.chunk.js"
+  },
+  {
+    "revision": "9cccd4a1edc7962b009f",
+    "url": "/typescript-exercises/static/js/39.968242ca.chunk.js"
+  },
+  {
+    "revision": "157a3dd6892529864962",
+    "url": "/typescript-exercises/static/js/4.da0fd6ac.chunk.js"
+  },
+  {
+    "revision": "53e867384ba2f6824e17",
+    "url": "/typescript-exercises/static/js/40.70f4e088.chunk.js"
+  },
+  {
+    "revision": "e19ee3ec7004b9699f32",
+    "url": "/typescript-exercises/static/js/41.00cc12d8.chunk.js"
+  },
+  {
+    "revision": "a643ac6a69de705da7ae",
+    "url": "/typescript-exercises/static/js/42.747a7a5f.chunk.js"
+  },
+  {
+    "revision": "a946e869caae85725b4d",
+    "url": "/typescript-exercises/static/js/43.7af1107c.chunk.js"
+  },
+  {
+    "revision": "ffa9c3771ce63b74572a",
+    "url": "/typescript-exercises/static/js/44.7ad0a7d6.chunk.js"
+  },
+  {
+    "revision": "8077ed8ed403aa96a6c1",
+    "url": "/typescript-exercises/static/js/45.18d4c83f.chunk.js"
+  },
+  {
+    "revision": "0f93fe0bfe3e3bf3bb4b",
+    "url": "/typescript-exercises/static/js/46.a857c595.chunk.js"
+  },
+  {
+    "revision": "572dca6b892b6be39c9f",
+    "url": "/typescript-exercises/static/js/47.9b1bf4b9.chunk.js"
+  },
+  {
+    "revision": "acce3c53ba32cd5b828b",
+    "url": "/typescript-exercises/static/js/48.1c8e9eee.chunk.js"
+  },
+  {
+    "revision": "35f74b58c8b82cf9de75",
+    "url": "/typescript-exercises/static/js/49.44b1e51c.chunk.js"
+  },
+  {
+    "revision": "0bb2aecb53d8896e2c99",
+    "url": "/typescript-exercises/static/js/5.bad8142e.chunk.js"
+  },
+  {
+    "revision": "18b8cac610619801bf2f",
+    "url": "/typescript-exercises/static/js/50.0abaa352.chunk.js"
+  },
+  {
+    "revision": "9674dacfe69504275ec1",
+    "url": "/typescript-exercises/static/js/51.52e56917.chunk.js"
+  },
+  {
+    "revision": "d029c8963c6358dc4b4e",
+    "url": "/typescript-exercises/static/js/52.481e6fab.chunk.js"
+  },
+  {
+    "revision": "dd1e04c4244b54913f34",
+    "url": "/typescript-exercises/static/js/53.2d2be5f8.chunk.js"
+  },
+  {
+    "revision": "60354741f3222bfa330b",
+    "url": "/typescript-exercises/static/js/54.752d38ef.chunk.js"
+  },
+  {
+    "revision": "3e2bf64e5150eef88394",
+    "url": "/typescript-exercises/static/js/55.bc80bc43.chunk.js"
+  },
+  {
+    "revision": "8f3172745d3bb5a86518",
+    "url": "/typescript-exercises/static/js/56.6bfc4520.chunk.js"
+  },
+  {
+    "revision": "cd0392223f1c523aca4c",
+    "url": "/typescript-exercises/static/js/57.4a4194fa.chunk.js"
+  },
+  {
+    "revision": "c3db1b88574dd0d6b707",
+    "url": "/typescript-exercises/static/js/58.effc8945.chunk.js"
+  },
+  {
+    "revision": "df0c888e11d86f6fe28d",
+    "url": "/typescript-exercises/static/js/59.9d1b4a0d.chunk.js"
+  },
+  {
+    "revision": "cf224aa154060b2c3c86fa6ed5af6aa8",
+    "url": "/typescript-exercises/static/js/59.9d1b4a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "788b97d2c32b68339347",
+    "url": "/typescript-exercises/static/js/6.a6db1b29.chunk.js"
+  },
+  {
+    "revision": "d51f409b085ba319d068",
+    "url": "/typescript-exercises/static/js/60.5b51c1a4.chunk.js"
+  },
+  {
+    "revision": "c179a5507c1d826f5cbd",
+    "url": "/typescript-exercises/static/js/61.57761987.chunk.js"
+  },
+  {
+    "revision": "f2c9ece89d0d85150f38",
+    "url": "/typescript-exercises/static/js/62.b5c545f0.chunk.js"
+  },
+  {
+    "revision": "b18a85dbcb3ba08f4924",
+    "url": "/typescript-exercises/static/js/63.f5da7508.chunk.js"
+  },
+  {
+    "revision": "b294f0d4346dda00a965",
+    "url": "/typescript-exercises/static/js/64.a2a8ff05.chunk.js"
+  },
+  {
+    "revision": "339da20f7ac3acf37583",
+    "url": "/typescript-exercises/static/js/65.b1e6a52c.chunk.js"
+  },
+  {
+    "revision": "08357ead8c0ed7bf1666",
+    "url": "/typescript-exercises/static/js/66.0b3bd24e.chunk.js"
+  },
+  {
+    "revision": "5730b06b2596cfc27389",
+    "url": "/typescript-exercises/static/js/7.2014e610.chunk.js"
+  },
+  {
+    "revision": "79439990ebcd9053132b",
+    "url": "/typescript-exercises/static/js/8.5640d761.chunk.js"
+  },
+  {
+    "revision": "62570570c13b259c79e4",
+    "url": "/typescript-exercises/static/js/9.abbbf6d8.chunk.js"
+  },
+  {
+    "revision": "7195d7e5b507246d1fee",
+    "url": "/typescript-exercises/static/js/main.7d49d1a2.chunk.js"
+  },
+  {
+    "revision": "9d3a96e12900dd31fc42",
+    "url": "/typescript-exercises/static/js/runtime-main.7f38587b.js"
+  },
+  {
+    "revision": "a609dc0f334a7d4e64205247c4e8b97c",
+    "url": "/typescript-exercises/static/media/codicon.a609dc0f.ttf"
+  },
+  {
+    "revision": "323f71e9b320b95d44cb3a9c02a42b69",
+    "url": "/typescript-exercises/ts.worker.js"
+  },
+  {
+    "revision": "ce15c9d372cc5baeacdeb733817bdde4",
+    "url": "/typescript-exercises/ts.worker.js.LICENSE.txt"
+  }
+]);
